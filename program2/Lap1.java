f58c3acc6c94294a3a711247fc71dccb75055720
Phoomss <narongsak5az@gmail.com>
2024-12-19 12:21:56 +0700

    lap1

---
+++ program2/Lap1.java
@@ -0,0 +1,21 @@
+public class Lap1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("Sum of a and b is " + c);
+
+        int d = a - b;
+        System.out.println("Difference of a and b is " + d);
+
+        byte e = 10;
+        byte f = 32000;
+        byte g = (byte) (e + f);
+        System.out.println("Sum of e and f is " + g);
+
+        byte h = (byte) (e - f);
+        System.out.println("Difference of e and f is " + h);
+
+
+    }
+}
