7148e3476cea2491cccc699e09079028316379f2
Phoomss <narongsak5az@gmail.com>
2025-02-20 10:56:28 +0700

    array two three index

---
+++ ArrayProgram/src/ArrayTwoIndex.java
@@ -0,0 +1,13 @@
+public class ArrayTwoIndex {
+    public static void main(String[] args) {
+        int[][] arry = {
+                { 1, 2, 3, 4, 5 },
+                { 6, 7, 8, 9, 10 },
+                { 11, 12, 13, 14, 15 },
+                { 16, 17, 18, 19, 20 }
+        };
+
+        int show = arry[2][1];
+        System.out.println(show);
+    }
+}
---
+++ ArrayProgram/src/SortArray.java
@@ -0,0 +1,41 @@
+import java.text.Collator;
+import java.util.Arrays;
+import java.util.Locale;
+
+public class SortArray {
+    public static void main(String[] args) {
+        // int[] arry = { 51, 3, 8, 20, 14 };
+        // Arrays.sort(arry);
+
+        // int index = Arrays.binarySearch(arry, 51);
+        // for (int i = 0; i < arry.length; i++) {
+        //     System.out.println(arry[i]);
+        // }
+        
+        // System.out.println("Index of 51: " + index);
+
+        // String[] arry = { "Justins", "jame", "johe", "am", "albit", "cambal" };
+        // Arrays.sort(arry);
+
+        // int index = Arrays.binarySearch(arry, "Justins");
+        // for (int i = 0; i < arry.length; i++) {
+        //     System.out.println(arry[i]);
+        // }
+        // System.out.println("Index of Z: " + index);
+      
+        // ------------------------------------------------//
+
+        // String[] name = {"ไก่", "หมู", "วัว", "แกะ", "แมว", "หมา"};
+
+        // Collator myCollator = Collator.getInstance(new Locale("th", "TH"));
+        // Arrays.sort(name, myCollator);
+
+        // int index = Arrays.binarySearch(name, "แมว", myCollator);
+        // for (int i = 0; i < name.length; i++) {
+        //     System.out.println(name[i]);
+        // }
+
+        // System.out.println("Index of แมว: " + index);
+
+    }
+}
